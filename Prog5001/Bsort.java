806b9e4d13d30eee2e3709c4dc33807cb168dd23
Adit0302 <a.selly.10@student.scu.edu.au>
2024-11-10 14:54:54 +0530

    Add files via upload

---
+++ Prog5001/Bsort.java
@@ -0,0 +1,26 @@
+package Prog5001;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Write a description of class Functions here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Bsort
+{
+    // Bubble sort algorithm to arrange marks in ascending order
+    public static void bSort(ArrayList<Double> marks) {
+        int n = marks.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (marks.get(j) > marks.get(j + 1)) {
+                    double temp = marks.get(j);
+                    marks.set(j, marks.get(j + 1));
+                    marks.set(j + 1, temp);
+                }
+            }
+        }
+    }
+}
---
+++ Prog5001/Compute.java
@@ -0,0 +1,30 @@
+package Prog5001;
+
+import java.util.ArrayList;
+/**
+ * Write a description of class computeMean here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Compute
+{
+       // Compute mean of the marks
+    public static double mean(ArrayList<Double> marks) {
+        double total = 0;
+        for (double mark : marks) {
+            total += mark;
+        }
+        return total / marks.size();
+    }
+          // Calculate standard deviation
+    public static double standardDeviation(ArrayList<Double> marks, double mean) {
+        double sumSquaredDiff = 0;
+        for (double mark : marks) {
+            double diff = mark - mean;
+            sumSquaredDiff += diff * diff;
+        }
+        double variance = sumSquaredDiff / marks.size();
+        return Math.pow(variance, 0.5); 
+    }
+}
